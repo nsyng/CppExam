9a317a061cd8345adefff15cc5355b595772217c
v <s>
2022-12-17 09:57:14 +0100

    Initial Commit

---
+++ CppExamSynhaivskyi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppExamSynhaivskyi
+//
+//  Created by Nazarii Synhaivskyi on 17.12.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
